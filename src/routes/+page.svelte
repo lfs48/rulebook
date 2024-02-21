d709e8241c90508170b35d595341697c7c058147
Lucas <lucasfschraier@gmail.com>
2024-02-21 16:29:00 -0500

    feat: add main element with chapters and headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+
+<section class='chapter'>
+
+<h1>Chapter 1: Characters</h1>
+
+    <section>
+
+    <h2>Player Characters</h2>
+        <section>
+            <p>
+            You take on the role of a <b>player character (PC)</b>. 
+            PCs are heroic (or perhaps antiheroic) adventurers
+            who brave perilous dangers seeking fame and fortune. 
+            Throughout this document, the word <i>you</i> will
+            refer primarily to the character you are controlling.
+            </p>
+
+            <p>
+            The following are some of the most important traits that define a PC:
+            </p>
+
+            <ul>
+                <li><b>Race</b> — their biological or magical species.</li>
+                <li><b>Class</b> — their archetypal role, which will determine most of their gameplay.</li>
+                <li><b>Level</b> — a number representing how powerful and experienced they are.</li>
+                <li><b>Attributes</b> — numbers that quantify their strengths and weaknesses.</li>
+                <li>Their appearance, personality, motivations, flaws, and other traits as a character within a story.</li>
+            </ul>
+
+        </section>
+
+    </section>
+
+</section>
+
+</main>
