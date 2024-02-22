112bff30c85eb2405c6a34ec628ab91ecaf71c15
Lucas <lucasfschraier@gmail.com>
2024-02-22 18:54:00 -0500

    content: edit player character section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,15 +54,26 @@
 
         <br/>
         
-        The following are some of the most important traits that define a PC:
+        The following are some of the most important traits that define your PC:
         </p>
 
         <ul>
-            <li><b>Race</b> — their biological or magical species.</li>
-            <li><b>Class</b> — their archetypal role, which will determine most of their gameplay.</li>
-            <li><b>Level</b> — a number representing how powerful and experienced they are.</li>
-            <li><b>Attributes</b> — numbers that quantify their strengths and weaknesses.</li>
-            <li>Their appearance, personality, motivations, flaws, and other traits as a character within a story.</li>
+            <li><b>Race</b> — your biological or supernatural species.
+                Your choice of race will grant you racial <b>features</b>
+                based on your physiology and innate magical abilities. 
+                It also determines your <b>size</b> and <b>creature types</b>.
+                A list of common races and their descriptions
+                can be found in Chapter 2: Races.
+            </li>
+            <li><b>Class</b> — your archetypal role, which will determine core aspects of your gameplay. 
+                A list of classes and their descriptions can be found in Chapter 3: Classes. 
+            </li>
+            <li><b>Level</b> — a number representing how powerful and experienced you are.
+                You typically start at level 1, and may eventually reach the maximum level of 10.
+                You increase your level by going on adventures and confronting challenges to earn <b>Experience</b>.
+            </li>
+            <li><b>Attributes</b> — numbers that quantify your strengths and weaknesses.</li>
+            <li>Your appearance, personality, motivations, flaws, and other traits as a character within a story.</li>
         </ul>
 
     </section>
