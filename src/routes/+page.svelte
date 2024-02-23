40c5fef897642e0e9abf613e37937c610fa46b9c
Lucas <lucasfschraier@gmail.com>
2024-02-23 08:38:00 -0500

    content: add leveling & experience section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -139,11 +139,100 @@
 
                 <br/>
 
-                Your attributes improve as you level up,
-                as detailed in the <b>Levels & Experience</b> section.
+                At 3rd, 5th, 7th, and 9th level, 
+                choose one of your attributes to increase by 1
+                (up to a maximum of 4).
             </p>
         </aside>
 
+    </section>
+
+    <section>
+
+        <h2>Levels & Experience</h2>
+
+        <p>
+            Your level is a number quantifying your experience
+            as an adventurer and the achievements under your belt.
+            You will typically begin the game as a 1st level character,
+            though you may sometimes be tasked with creating a higher level
+            character from scratch — such as if your previous character dies.
+            The maximum level you can reach is 10th.
+
+            <br/>
+
+            As you go on adventures and overcome challenges,
+            you will earn <b>experience points (EXP)</b>.
+            EXP is awarded based on meeting certain criteria during an adventure.
+            When you complete downtime, you will earn 1 EXP if
+            your party achieved at least two of the following since your last downtime.
+            If you achieved three or more, you earn 2 EXP.
+        </p>
+
+        <ul>
+            <li>Bested a challenging foe in combat.</li>
+            <li>Plundered a valuable and well-guarded treasure.</li>
+            <li>Unraveled a mystery, foiled a plot, or unmasked a conspiracy.</li>
+            <li>Navigated a perilous situation with a clever plan.</li>
+            <li>Achieved something of great personal significance not otherwise covered by these criteria.</li>
+        </ul>
+
+        <p>
+            Once you have earned a sufficient number of EXP,
+            your level increases by 1. This is called <b>leveling up</b>.
+            Leveling up from 1st to 2nd level requires 3 EXP,
+            and each subsequent level requires one additional EXP
+            (2nd→3rd requires 4, 3rd→4th requires 5, etc).
+            Your EXP resets when you level up.
+            Any leftover EXP you have over the amount required to level up
+            doesn't carry over to the next level,
+            and is instead converted to <b>fate points</b>.
+        </p>
+
+        <aside>
+            <h3>Tiers of Play</h3>
+            <p>
+                Levels are divided into four tiers,
+                representing overarching stages of your adventuring career.
+            </p>
+            <ul>
+                <li>
+                    <b>Tier 1 — Novice Adventurers (Levels 1-3).</b>
+                    As a newly minted adventurer, you have yet to make a name for yourself.
+                    Your adventures largely consist of helping individual people with localized problems,
+                    like fending off wild beasts and clearing bandit camps.
+                </li>
+                <li>
+                    <b>Tier 2: Local Heroes (Levels 4-6).</b>
+                    You have proven yourself a competent adventurer
+                    and are well known to the communities you’ve helped.
+                    The scope of your adventures expands in both danger and significance.
+                    You’ll face off against dangerous monsters like gorgons and minotaurs,
+                    while delving for fabled treasures or currying favor with the lady of the realm.
+                </li>
+                <li>
+                    <b>Tier 3: Renowned Champions (Levels 7-9).</b>
+                    You have set yourself high above ordinary folk.
+                    Rumors of your deeds are spoken far and wide throughout the land,
+                    and you can expect your reputation to precede you wherever you go.
+                    You slay dragons, save kingdoms, and unravel ancient mysteries.
+                </li>
+                <li>
+                    <b>Tier 4: Epic Paragons (Level 10+).</b> 
+                    You are at the apex of your power and influence.
+                    Stories of your legendary accomplishments have spread to every corner of the map.
+                    Demon lords and ancient eldritch evils factor you into their diabolical plots.
+                    Your actions shape the fate of the world, and perhaps even the multiverse.
+                </li>
+            </ul>
+            <p>
+                Transitioning between tiers of play represents a substantial shift
+                in the style and tone of the game, with an exponential increase
+                in the complexity and stakes of the challenges before you.
+                Thus each tier of play will usually encompass its own narrative arc.
+            </p>
+        </aside>
+        
     </section>
 
 </section>
