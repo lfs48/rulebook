0f8c76cfd3f879cb39e016d85cb2371e77882a5c
Lucas <lucasfschraier@gmail.com>
2024-04-09 17:13:22 -0400

    feat: add vertical navbar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,30 @@
     import "@fontsource/nunito";
 </script>
 
+<style>
+    nav {
+        @apply
+            fixed
+            left-0
+            top-0
+            w-24
+            h-full
+            flex
+            flex-col
+            bg-blue-800
+            p-4
+            text-white
+            text-sm
+            font-semibold
+            space-y-2
+    }
+</style>
+
+<nav>
+    <h5>Contents</h5>
+    <div>Characters</div>
+    <div>Races</div>
+    <div>Classes</div>
+</nav>
+
 <slot />
